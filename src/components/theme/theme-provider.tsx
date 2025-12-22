5b895c8186d9cf35f405c619691fc6e6af332f84
Zoltan Erdos <zoltan.erdos@me.com>
2025-12-22 17:01:14 +0000

    fix: save

--- src/components/theme/theme-provider.tsx
+++ src/components/theme/theme-provider.tsx
@@ -3,6 +3,15 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { ThemeProviderProps } from "next-themes";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+interface ExtendedThemeProviderProps extends ThemeProviderProps {
+  /** CSP nonce for inline script execution */
+  nonce?: string;
+}
+
+export function ThemeProvider({ children, nonce, ...props }: ExtendedThemeProviderProps) {
+  return (
+    <NextThemesProvider nonce={nonce} {...props}>
+      {children}
+    </NextThemesProvider>
+  );
 }
