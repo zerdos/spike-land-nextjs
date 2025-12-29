bc967eec5e349e6e6ad83672b51f97aa9d8f2c7e
Zoltan Erdos <zoltan.erdos@me.com>
2025-12-29 19:44:54 +0000

    feat: Enhance card component styling with gradients, hover effects, and z-indexing, and slightly increase slider thumb size.

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -20,7 +20,7 @@ const Slider = React.forwardRef<
     <SliderPrimitive.Track className="relative h-2.5 w-full grow overflow-hidden rounded-full bg-white/25 shadow-inner">
       <SliderPrimitive.Range className="absolute h-full bg-primary" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-11 w-11 md:h-6 md:w-6 rounded-full border-2 border-background bg-primary shadow-lg ring-offset-background transition-transform hover:scale-110 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
+    <SliderPrimitive.Thumb className="block h-11 w-11 md:h-7 md:w-7 rounded-full border-2 border-background bg-primary shadow-lg ring-offset-background transition-transform hover:scale-110 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
